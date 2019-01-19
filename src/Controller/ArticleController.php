0fbc60165c4af28c3ba4bc1343e710dd1abab492
vinset <v.quaranta@cinetyk.com>
2019-01-19 19:32:37 +0100

    Add Bootstrap Italia

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,13 +3,13 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ArticleController extends AbstractController
 {
     /**
-     * @Route("/")
+     * @Route("/", name="app_homepage")
      */
     public function homepage()
     {
