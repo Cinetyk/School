c8ae2f8f70249b8fb016a7ca6714ca447684d950
vinset <v.quaranta@cinetyk.com>
2019-01-19 09:53:05 +0100

    piccoli avanzamenti

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class ArticleController
+{
+    /**
+     * @Route("/")
+     */
+    public function homepage()
+    {
+        return new Response('Tutto pronto');
+    }
+
+    /**
+     * @Route("/news/{slug}")  
+     */
+    public function show($slug)
+    {
+        return new Response(sprintf(
+            'Articolo %s',
+            $slug
+        ));
+    }
+}
