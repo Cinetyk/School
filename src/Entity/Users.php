f1d1192aaff084073a29fb4b12c525c7261a23b4
vinset <v.quaranta@cinetyk.com>
2019-02-16 17:01:38 +0100

    Tabella utenti

--- src/Entity/Users.php
+++ src/Entity/Users.php
@@ -26,6 +26,16 @@ class Users
      */
     private $surname;
 
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $username;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $password;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -51,6 +61,30 @@ class Users
     public function setSurname(string $surname): self
     {
         $this->surname = $surname;
+
+        return $this;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
 
         return $this;
     }
